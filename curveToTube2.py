ac25e8287c9b4b059b8d38aaabb872a43fed8812
Phi Hung Nguyen <josephkirk.art@gmail.com>
2017-02-07 18:24:20 +0700

    curvetotube2

--- curveToTube2.py
+++ curveToTube2.py
@@ -1,5 +1,13 @@
 import pymel.core as pm
 import maya.cmds as cmds
+def createPointParent(ob,name="PointParent#"):
+    obPos= pm.xform(ob,q=1,ws=1,t=1)
+    obOldParent = ob.getParent()
+    obNewParent = pm.spaceLocator(n=name)
+    obNewParent.setAttr('translate',obPos)
+    if obOldParent:
+        pm.parent(obNewParent,obOldParent)
+    pm.parent(ob,obNewParent)
 def makeCurveTube():
     pathTransform = pm.selected()
     if not pathTransform:
@@ -8,23 +16,32 @@ def makeCurveTube():
     pathShape = pm.listRelatives(shapes=True)
     if pm.nodeType(pathShape[0])=='nurbsCurve':
         pathCurve=[(i,pathShape[pathTransform.index(i)]) for i in pathTransform]
-        profileCurve = pm.circle(c=(0,0,0), nr=(0,1,0), sw=360, r=1, d=3, ut=0, tol=5.77201e-008, s=8, ch=1, name='extrudeprofileCurve#')
+        if pm.objExists('HairBaseProfileCurve'):
+            profileCurve = ls('HairBaseProfileCurve')[0]
+        else:
+            profileCurve = pm.circle(c=(0,0,0), nr=(0,1,0), sw=360, r=1, d=3, ut=0, tol=5.77201e-008, s=8, ch=1, name='HairBaseProfileCurve')
+        pm.select(d=1)
         for crv in pathCurve:
             #profileInstance = instance(profileCurve,n="pCrv_instance"+string(pathCurve.index(crv)))
+            hairOncGroup=pm.group(n="HairCtrGroup#")
+            pm.parent(hairOncGroup,crv[0])
             mPath=pm.pathAnimation(profileCurve,crv[0],fa='y',ua='x')
             HairProfile =[]
             for u in range(4):
                 pm.setAttr(mPath+".uValue",float(u))
-                profileInstance = pm.instance(profileCurve,n="proFileCrv_"+str(u))
+                profileInstance = pm.instance(profileCurve,n=crv[0]+"HairProFileCrv_"+str(u))
+                pm.parent(profileInstance,hairOncGroup)
                 if u!= 2:
                     profileInstance[0].setAttr('scale',(0.01,0.01,0.01))
                 if u==0:
                     pm.delete(profileInstance)
                 else:
                     HairProfile.append(profileInstance)
+            createPointParent(HairProfile[0][0],name=crv[0]+"HairRoot_Ctrl_"+str(pathCurve.index(crv)))
+            createPointParent(HairProfile[len(HairProfile)-1][0],name=crv[0]+"HairTop_Ctrl_"+str(pathCurve.index(crv)))
             pm.select(HairProfile)
             pm.nurbsToPolygonsPref(pt=1,un=4,vn=7)
-            HairMesh=pm.loft(n="HairMesh",po=1,ch=1,u=1,c=0,ar=1,d=3,ss=1,rn=0,rsn=True)
+            HairMesh=pm.loft(n="HairMesh#",po=1,ch=1,u=1,c=0,ar=1,d=3,ss=1,rn=0,rsn=True)
             #print HairMesh
             if str(cmds.getAttr('defaultRenderGlobals.ren'))=='vray':
                 HairShape = pm.listRelatives(HairMesh[0],shapes=True)
@@ -46,5 +63,23 @@ def makeCurveTube():
             HairMesh[0].connectAttr('widthDivisions',HairTess+".uNumber")
             HairMesh[0].connectAttr('lengthDivisions',HairTess+".vNumber")
             pm.delete(mPath)
-        pm.delete(profileCurve)
+            pm.select(d=1)
+        pm.hide(profileCurve)
+def cleanHairMesh():
+    newAttr =['width','orientation','lengthDivisions','widthDivisions']
+    meshes = pm.ls("HairMesh*")
+    HairGroup = pm.ls("HairCtrGroup*")
+    for mesh in meshes:
+        pm.delete(mesh,ch=True)
+    for o in HairGroup:
+        if pm.objExists(o):
+           pm. delete(o)
+    if pm.objExists("HairBaseProfileCurve"):
+        pm. delete("HairBaseProfileCurve")
+    for mesh in meshes:
+        print mesh
+        for a in newAttr:
+            print a
+            if (pm.attributeQuery (a, exists=1, node=mesh)):
+                pm.deleteAttr (mesh+"."+a)
 
