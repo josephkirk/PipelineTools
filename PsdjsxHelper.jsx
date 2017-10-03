b215490acb88b3e3bd21959ade76ed0a37fc0255
Phi Hung Nguyen <josephkirk.art@gmail.com>
2017-10-03 13:26:56 +0700

    cleanup

--- PsdjsxHelper.jsx
+++
@@ -1,3 +0,0 @@
-var newDoc = app.documents.add()
-var newLayer = newDoc.artLayers.add()
-newLayer.kind = LayerKind.SOLIDFILL
--- colors/MayaRun.py
+++
@@ -1,5 +0,0 @@
-import sys
-sys.path.append(r'C:\Program Files\Quixel SUITE 2.0\colors')
-
-import main
-reload(main.main())
