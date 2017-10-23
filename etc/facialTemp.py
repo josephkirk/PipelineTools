96e778f5b6bfb7e14be97397976008a1994c67d1
Phi Hung Nguyen <josephkirk.art@gmail.com>
2017-10-23 18:30:26 +0700

    scriptNode add

--- etc/facialTemp.py
+++ etc/facialTemp.py
@@ -615,3 +615,24 @@ def create_facial_rig():
         return
     pm.informBox(title='Riggin Status', message = "Face Rig Complete")
 
+def facialScriptNode():
+    def connectBS():
+        try:
+            if not pm.ls('RootBS') and not pm.ls('EyeDeformBS'):
+                pm.select('facial',r=True)
+                pm.select("*_face_grp_skinDeform",add=True)
+                pm.blendShape(name='RootBS',w=[(0,1),], automatic=True)
+                pm.select('eyeDeform',r=True)
+                pm.select("*_eye_grp_skinDeform",add=True)
+                pm.blendShape(name='EyeDeformBS',w=[(0,1),], ar=True)
+        except:
+            print 'Cant connect BS'
+    def unConnectBS():
+        try:
+            pm.delete('RootBS')
+            pm.delete('EyeDeformBS')
+        except:
+            'cant delete RootBS and EyeDeformBS'
+    initNode = pm.scriptNode( st=2, bs='connectBS()', n='script', stp='python')
+    pm.scriptNode(initNode, e=True, afterScript='unConnectBS()', stp='python'
+    return initNode 
